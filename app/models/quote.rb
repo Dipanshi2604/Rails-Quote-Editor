465771e54c54a22fd347abb94c5142dd02101a0f
User34 <dipanshi.prajapat@gemsessence.com>
2024-03-12 15:52:20 +0530

    adding all functionality

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,8 @@
 class Quote < ApplicationRecord
   belongs_to :company
   has_many :line_item_dates, dependent: :destroy
-
+  has_many :line_items, through: :line_item_dates
+  
   validates :name, presence: true
   scope :ordered, -> { order(id: :desc) }
 
@@ -12,4 +13,8 @@ class Quote < ApplicationRecord
 
   # Those three callbacks are equivalent to the following single line
   #  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
